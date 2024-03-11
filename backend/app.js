ee06d0a33161d55acea09331916077b87a83b01f
ShyLuns <drpg1008@gmail.com>
2024-03-11 08:52:17 -0500

    Se importo el controlador de novedades y las rutas para su uso

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import usuarioRouter from './routes/usuario.router.js';
 import prestamosRouter from './routes/prestamos.router.js';
 import ambientesRouter from './routes/ambientes.router.js';
+import novedadesRouter from './routes/novedades.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(usuarioRouter);
 app.use(prestamosRouter);
 app.use(ambientesRouter);
+app.use(novedadesRouter);
 
 //start server
 
